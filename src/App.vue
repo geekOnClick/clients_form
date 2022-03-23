ca2321f218191c0cdc263f3f0bf9e6993b2a8fec
Alexander Kozlov <kozloww1508@yandex.ru>
2022-03-23 12:08:08 +0300

    start app

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <div class="wrap">
+          Hello
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+};
+</script>
+
+<style></style>
